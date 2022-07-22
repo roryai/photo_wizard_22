71af768c6dd326a55418b309826065d6468e0725
Roar A <rory@Roars-MacBook-Air.local>
2022-07-22 11:04:05 +0100

    feat: adds file size to record

--- spec/wizard_spec.rb
+++ spec/wizard_spec.rb
@@ -2,11 +2,13 @@ require_relative 'spec_helper'
 require_relative '../wizard'
 
 RSpec.describe do
+  photo_1_path = "/Users/rory/code/transfer_wizard_22/spec/test_photos/small_cat.jpg"
 
   it 'adds file path of photo to list of files to be transferred' do
-    wiz = Wizard.new
-
-    expect(wiz.scan).to include "/Users/rory/code/transfer_wizard_22/spec/test_photos/small_cat.jpg"
+    expect(Wizard.new.scan.first).to include photo_1_path
   end
 
+  it 'adds file size to record' do
+    expect(Wizard.new.scan.first).to include 12429
+  end
 end
--- wizard.rb
+++ wizard.rb
@@ -1,7 +1,18 @@
 class Wizard
 
-  def hello
-    "Hello, world!"
+  SOURCE = "/Users/rory/code/transfer_wizard_22/spec/test_photos"
+
+  def scan
+    Dir.chdir(SOURCE)
+    file_names = Dir.children(SOURCE)
+
+    records = []
+
+    file_names.each do |f|
+      records << [File.absolute_path(f), File.size(f)]
+    end
+
+    records
   end
 
 end
