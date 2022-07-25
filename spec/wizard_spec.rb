70fef411a7508a431b910bd8ff0307eda664e322
Roar A <rory@Roars-MacBook-Air.local>
2022-07-25 17:47:59 +0100

    feat: add logging

--- spec/wizard_spec.rb
+++ spec/wizard_spec.rb
@@ -3,16 +3,30 @@ require_relative "../wizard.rb"
 require_relative "../file_record.rb"
 
 RSpec.describe do
-  photo_1_path = "/Users/rory/code/transfer_wizard_22/spec/test_photos/source/small_cat.jpg"
+  photo_1 = "small_cat.jpg"
+  source_directory = "/Users/rory/code/transfer_wizard_22/spec/test_photos/source"
   target_directory = "/Users/rory/code/transfer_wizard_22/spec/test_photos/target"
+  photo_1_source_path = source_directory + "/" + photo_1
+  photo_1_target_path = target_directory + "/" + photo_1
+  log_file = target_directory + "/" + "file_wizard_log.txt"
+
+  before(:each) do
+    Dir.chdir(target_directory)
+    Dir.children(target_directory).each do |file|
+      File.delete(file)
+    end
+
+    log_file = "/Users/rory/code/transfer_wizard_22/spec/test_photos/target/file_wizard_log.txt"
+    File.open(log_file, "w") # wipe_log_file
+  end
 
   describe "records" do
     it "adds file name to record" do
-      expect(Wizard.new.scan.first.name).to eq "small_cat.jpg"
+      expect(Wizard.new.scan.first.name).to eq photo_1
     end
 
-    it "adds file path of photo to record" do
-      expect(Wizard.new.scan.first.path).to eq photo_1_path
+    it "adds file path source of photo to record" do
+      expect(Wizard.new.scan.first.path).to eq source_directory + "/" + photo_1
     end
 
     it "adds file size to record" do
@@ -21,28 +35,45 @@ RSpec.describe do
   end
 
   describe "transfers" do
-    it "ignores files that are not .jpg" do
-      records = Wizard.new.scan
-      expect(records.first.name).to eq "small_cat.jpg"
-      expect(records.size).to eq 1
-    end
-
     it "copies a jpg to the source directory" do
       Wizard.new.transfer
       Dir.chdir(target_directory)
-      expect(File.exist?(photo_1_path)).to be true
+
+      expect(File.exist?(photo_1)).to be true
     end
 
     it "does not copy a file with no extension" do
       Wizard.new.transfer
       Dir.chdir(target_directory)
+
       expect(File.exist?("no_ext_file")).to be false
     end
 
     it "does not copy a directory" do
       Wizard.new.transfer
       Dir.chdir(target_directory)
+
       expect(Dir.exist?("empty_dir")).to be false
+    end
+  end
+
+  describe "logging" do
+    it "creates a log entry for a file that alread exists in target dir" do
+      Wizard.new.transfer
+      Wizard.new.transfer
+
+      Dir.chdir(target_directory)
+      log = File.open(log_file).read
+
+      expect(log).to eq "Copied " + photo_1_source_path + " to " + photo_1_target_path + "\n" + "File " + photo_1 + " already exists in " + target_directory + "\n"
+    end
+
+    it "creates a log entry for a transferred file" do
+      Wizard.new.transfer
+      Dir.chdir(target_directory)
+      log = File.open(log_file, &:readline)
+
+      expect(log).to eq "Copied " + photo_1_source_path + " to " + photo_1_target_path + "\n"
     end
   end
 end
--- wizard.rb
+++ wizard.rb
@@ -6,6 +6,7 @@ class Wizard
 
   SOURCE_DIR = "/Users/rory/code/transfer_wizard_22/spec/test_photos/source"
   TARGET_DIR = "/Users/rory/code/transfer_wizard_22/spec/test_photos/target"
+  LOG_DIR = TARGET_DIR + "/" + "file_wizard_log.txt"
 
   def scan
     Dir.chdir(SOURCE_DIR)
@@ -24,7 +25,16 @@ class Wizard
 
   def transfer
     scan.each do |record|
+      if File.exist?(TARGET_DIR + "/" + record.name)
+        log_already_exists(record.name)
+        next
+      end
+
       FileUtils.cp(record.path, TARGET_DIR)
+
+      if File.exist?(TARGET_DIR + "/" + record.name)
+        log_success(record.name)
+      end
     end
   end
 
@@ -32,11 +42,23 @@ class Wizard
 
   def filter(file_names)
     filtered_file_names = []
-    
+
     file_names.each do |file_name|
       filtered_file_names << file_name if File.extname(file_name) == ".jpg"
     end
 
     filtered_file_names
+  end
+
+  def log_already_exists(file_name)
+    File.open(LOG_DIR, "a+") do |file|
+      file.write("File " + file_name + " already exists in " + TARGET_DIR + "\n")
+    end
+  end
+
+  def log_success(file_name)
+    File.open(LOG_DIR, "a+") do |file|
+      file.write("Copied " + SOURCE_DIR + "/" + file_name + " to " + TARGET_DIR + "/" + file_name + "\n")
+    end
   end
 end
